3c39c3b5b3c675c06d09822150324f0868c1f27c
Robert Gajewski <robert.gajewski@nask.pl>
2021-06-16 15:55:13 +0200

    kodilla-course-commit fasade

--- kodilla-testing/src/test/java/com/kodilla/testing/library/BookDirectoryTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/library/BookDirectoryTestSuite.java
@@ -148,7 +148,8 @@ public class BookDirectoryTestSuite {
     private List<Book> generateListOfNBooks(int booksQuantity) {
         List<Book> resultList = new ArrayList<>();
         for (int n = 1; n <= booksQuantity; n++) {
-            Book theBook = new Book("Title " + n, "Author " + n, 1970 + n);
+            Book theBook = new Book
+                    ("Title " + n, "Author " + n, 1970 + n);
             resultList.add(theBook);
         }
         return resultList;
