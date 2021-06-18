2734529d2cdae6f29dd5db8b278f50b91f62946e
Robert Gajewski <robert.gajewski@nask.pl>
2021-06-18 23:46:35 +0200

    kodilla-course-commit Observer homework updated

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Task.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/homework/Task.java
@@ -1,16 +1,16 @@
 package com.kodilla.patterns2.observer.homework;
 
 public class Task {
-    private final String name;
+    private final String taskname;
     private  Status status;
 
     public Task(String name) {
-        this.name = name;
-        this.status = Status.WAIT;
+        this.taskname = name;
+        this.status = Status.WAIT; /* default */
     }
 
-    public String getName() {
-        return name;
+    public String getTaskname() {
+        return taskname;
     }
 
     public Status getStatus() {
